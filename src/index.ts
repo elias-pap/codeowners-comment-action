5c633a585ed45219b270d0825b907ef0f1ef47ce
Elias Papavasileiou <eliaspap@pm.me>
2025-11-09 18:57:10 +0100

    feat: improve logging

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,7 @@ const main = async () => {
   const changedFiles = await getChangedFiles();
   if (!changedFiles) return logError("No changed files found.");
 
-  logDebug(`Detected changed files:\n${changedFiles.join("\n")}`);
+  logDebug(`Detected changed files:\n-> ${changedFiles.join("\n-> ")}`);
 };
 
 main();
